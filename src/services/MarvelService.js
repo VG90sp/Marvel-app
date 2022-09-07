4ee14437383de553002da84c20b22175fd486079
Vladimir Grigoriev <Vladimir.Grigoriev@volnamobile.ru>
2022-09-07 16:41:30 +0300

    initial commit

---
+++ src/services/MarvelService.js
@@ -0,0 +1,24 @@
+class MarvelService {
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=7c72069f40aae9eedaa541387ddd1d28";
+
+  getResourse = async (url) => {
+    let res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+  getAllCharacters = () => {
+    return this.getResourse(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
+  };
+
+  getCharacter = (id) => {
+    return this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  };
+}
+
+export default MarvelService;
